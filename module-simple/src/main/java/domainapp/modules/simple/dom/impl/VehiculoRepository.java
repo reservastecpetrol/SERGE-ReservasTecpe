bd00da0a4e85e7e624c45b3afcbc6ad30fdcb225
cintiaMillacura2207 <cg2207m@gmail.com>
2019-07-03 14:55:32 -0300

    #2 Se crea el repositorio de servicio
      En este commit se crea el repositorio de
      servicio correspondiente a la clase vehiculo.

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/VehiculoRepository.java
@@ -0,0 +1,81 @@
+package domainapp.modules.simple.dom.impl;
+
+import java.util.List;
+
+import org.datanucleus.query.typesafe.TypesafeQuery;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
+import org.apache.isis.applib.services.repository.RepositoryService;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        objectType = "simple.VehiculoMenu",
+        repositoryFor = Vehiculo.class
+)
+@DomainServiceLayout(
+        named = "Vehiculos",
+        menuOrder = "10"
+)
+
+public class VehiculoRepository {
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    @MemberOrder(sequence = "1")
+    public List<Vehiculo> listAll() {
+        return repositoryService.allInstances(Vehiculo.class);
+    }
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    @MemberOrder(sequence = "2")
+    public List<Vehiculo> findByMatricula(
+            @ParameterLayout(named="Matricula")
+            final String matricula
+    ) {
+        TypesafeQuery<Vehiculo> q = isisJdoSupport.newTypesafeQuery(Vehiculo.class);
+        final QVehiculo cand = QVehiculo.candidate();
+        q = q.filter(
+                cand.matricula.indexOf(q.stringParameter("matricula")).ne(-1)
+        );
+        return q.setParameter("matricula", matricula)
+                .executeList();
+    }
+
+    public static class CreateDomainEvent extends ActionDomainEvent<SimpleObjects> {}
+    @Action(domainEvent = SimpleObjects.CreateDomainEvent.class)
+    @MemberOrder(sequence = "3")
+    public Vehiculo create(
+            @ParameterLayout(named="Matricula") final String matricula,
+            @ParameterLayout(named="Marca")final String marca,
+            @ParameterLayout(named="Color")final String color,
+            @ParameterLayout(named="Modelo") final String modelo,
+            @ParameterLayout(named="Combustible") final boolean combustible,
+            @ParameterLayout(named="Seguro") final boolean seguro,
+            @ParameterLayout(named="Ubicacion")final String ubicacion,
+            @ParameterLayout(named="Estado")final String estado
+    )
+    {
+        return repositoryService.persist(new Vehiculo(matricula,marca,color,modelo,combustible,seguro,ubicacion,estado));
+    }
+
+
+    @javax.inject.Inject
+    RepositoryService repositoryService;
+
+    @javax.inject.Inject
+    IsisJdoSupport isisJdoSupport;
+
+    @javax.inject.Inject
+    org.apache.isis.applib.DomainObjectContainer container;
+}
