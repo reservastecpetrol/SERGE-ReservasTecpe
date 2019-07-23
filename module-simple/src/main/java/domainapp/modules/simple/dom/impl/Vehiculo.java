202cb3d70b55458071a635d314bd70a7fda1218a
cintiaMillacura2207 <cg2207m@gmail.com>
2019-07-23 13:57:22 -0300

    #40 Se agrego anotacion a la clase Vehiculo
      En este commit se agrego esta anotacion a la
      clase Vehiculo pues la misma es referenciada
      en el viewModel Compare.

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/Vehiculo.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/Vehiculo.java
@@ -9,6 +9,7 @@ import javax.jdo.annotations.Query;
 import javax.jdo.annotations.Unique;
 import javax.jdo.annotations.Version;
 import javax.jdo.annotations.VersionStrategy;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import com.google.common.collect.ComparisonChain;
 
@@ -26,6 +27,7 @@ import org.apache.isis.applib.services.i18n.TranslatableString;
 import org.apache.isis.applib.services.message.MessageService;
 import org.apache.isis.applib.services.repository.RepositoryService;
 import org.apache.isis.applib.services.title.TitleService;
+import org.apache.isis.schema.utils.jaxbadapters.PersistentEntityAdapter;
 
 import lombok.AccessLevel;
 import static org.apache.isis.applib.annotation.CommandReification.ENABLED;
@@ -67,6 +69,7 @@ import static org.apache.isis.applib.annotation.SemanticsOf.NON_IDEMPOTENT_ARE_Y
         bookmarking = BookmarkPolicy.AS_ROOT
 )
 @lombok.Getter @lombok.Setter
+@XmlJavaTypeAdapter(PersistentEntityAdapter.class)
 public class Vehiculo implements Comparable<Vehiculo> {
 
     @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
