cbad9fcfba67fc0a06ac47ff7a01b837db32dbaf
cintiaMillacura2207 <cg2207m@gmail.com>
2019-09-09 20:44:35 -0300

    Issue #50:Se agrega documentacion javadoc
      Se agrega la informacion necesaria para generar
      la documentacion javadoc.

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/Habitacion.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/Habitacion.java
@@ -67,28 +67,51 @@ import static org.apache.isis.applib.annotation.SemanticsOf.NON_IDEMPOTENT_ARE_Y
         bookmarking = BookmarkPolicy.AS_ROOT
 )
 @lombok.Getter @lombok.Setter
+
+/**
+ * Esta clase define la entidad de dominio Habitacion
+ * con todas sus propiedades.
+ * Ademas de metodos que realizan
+ * Validacion de propiedades
+ * Actualizacion de propiedades
+ * Eliminar una entidad de Habitacion
+ * Un metodo Constructor
+ *Metodos para modificar los estados de la entidad Habitacion
+ *
+ *
+ * @author Francisco Bellani
+ *
+ */
 public class Habitacion implements Comparable<Habitacion> {
+
+    //Definicion de las propiedades de la entidad Habitacion
 
     @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
     @lombok.NonNull
     @Property() // editing disabled by default, see isis.properties
     @Title(prepend = "Habitacion: ")
-    private String nombre;
+    private String nombre;  //esta variable hace referencia al numero que identifica a la entidad Habitacion
 
     @javax.jdo.annotations.Column(allowsNull = "true", length = 40)
     @lombok.NonNull
     @Property(editing = Editing.ENABLED)
-    private String ubicacion;
+    private String ubicacion;   //esta variable hace referencia a la ubicacion en que se encuentra la entidad Habitacion
 
     @javax.jdo.annotations.Column(allowsNull = "true", length = 40)
     @lombok.NonNull
     @Property(editing = Editing.ENABLED)
+    // esta variable hace referencia al estado en el que se encuentra la entidad Habiatcion
+    // los cuales pueden ser :
+    // DISPONIBLE|| OCUPADA || MANTENIMIENTO|| INACTIVA
     private String estado;
-    //listado de categorias de habitacion dropdown menu
-    private ListaHabitaciones categoria;
+
+
+    // esta variable hace referencia al tipo de categoria a la que pertenece la entidad Habitacion
+    // Ejecutivas || Estandar || Simple
     @javax.jdo.annotations.Column(allowsNull="true")
     @lombok.NonNull
     @Property(editing = Editing.ENABLED)
+    private ListaHabitaciones categoria;
     public ListaHabitaciones getCategoria() {
         return categoria;
     }
@@ -96,7 +119,15 @@ public class Habitacion implements Comparable<Habitacion> {
         this.categoria = categoria;
     }
 
-
+    /**
+     * Este es un metodo constructor
+     *
+     *
+     * @param nombre -valor ingresado por el usuario
+     * @param ubicacion -valor ingresado por el usuario
+     * @param categoria -valor ingresado por el usuario
+     * @param estado  -valor definido en el codigo
+     */
     Habitacion(String nombre,String ubicacion, ListaHabitaciones categoria,String estado){
         this.nombre=nombre;
         this.ubicacion=ubicacion;
@@ -104,6 +135,13 @@ public class Habitacion implements Comparable<Habitacion> {
         this.estado=estado;
         }
 
+
+    /**
+     * Este metodo realiza la actualizacion de la variable nombre
+     *
+      * @param nombre
+     * @return Habitacion
+     */
     @Action(semantics = IDEMPOTENT, command = ENABLED, publishing = Publishing.ENABLED, associateWith = "nombre")
     public Habitacion updateNombre(
             @Parameter(maxLength = 40)
@@ -112,6 +150,14 @@ public class Habitacion implements Comparable<Habitacion> {
         setNombre(nombre);
         return this;
     }
+
+
+    /**
+     * Este metodo realiza la actualizacion de la variable ubicacion
+     *
+     * @param ubicacion
+     * @return Habitacion
+     */
     @Action(semantics = IDEMPOTENT, command = ENABLED, publishing = Publishing.ENABLED, associateWith ="ubicacion")
     public Habitacion updateUbicacion(
             @Parameter(maxLength = 40)
@@ -121,6 +167,13 @@ public class Habitacion implements Comparable<Habitacion> {
         return this;
     }
 
+
+    /**
+     * Este metodo realiza la actualizacion de la variable categoria
+     *
+     * @param categoria
+     * @return Habitacion
+     */
     @Action(semantics = IDEMPOTENT, command = ENABLED, publishing = Publishing.ENABLED, associateWith = "categoria")
     public ListaHabitaciones updateCategoria(
             @Parameter(maxLength = 40)
@@ -130,6 +183,13 @@ public class Habitacion implements Comparable<Habitacion> {
         return categoria;
     }
 
+
+    /**
+     * Este metodo realiza la actualizacion de la variable estado
+     *
+     * @param estado
+     * @return Habitacion
+     */
     @Action(semantics = IDEMPOTENT, command = ENABLED, publishing = Publishing.ENABLED, associateWith ="estado")
     public Habitacion updateEstado(
             @Parameter(maxLength = 40)
@@ -144,6 +204,14 @@ public class Habitacion implements Comparable<Habitacion> {
         return getNombre();
     }
 
+
+    /**
+     * Este metodo realiza la validacion de la variable nombre
+     * de forma que el usuario no ingrese un signo de exclamacion
+     *
+     * @param nombre
+     * @return TranslatableString
+     */
     public TranslatableString validate0UpdateNombre(final String nombre) {
         return nombre != null && nombre.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
     }
@@ -152,16 +220,33 @@ public class Habitacion implements Comparable<Habitacion> {
         return getUbicacion();
     }
 
+
+    /**
+     * Este metodo realiza la validacion de la variable ubicacion
+     * de forma que el usuario no ingrese un signo de exclamacion
+     *
+     * @param ubicacion
+     * @return TranslatableString
+     */
     public TranslatableString validate0UpdateUbicacion(final String ubicacion) {
         return ubicacion != null && ubicacion.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
     }
 
+
+    /**
+     * Este metodo realiza la validacion de la variable estado
+     * de forma que el usuario no ingrese un signo de exclamacion
+     *
+     * @param estado
+     * @return TranslatableString
+     */
     public TranslatableString validate0UpdateEstado(final String estado) {
         return estado != null && estado.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
     }
 
+
     /**
-     * Este metodo cambia el valor de la variable estado a OCUPADO
+     * Este metodo cambia el valor de la variable estado a OCUPADA
      */
     public void ocupada()
     {
@@ -170,22 +255,23 @@ public class Habitacion implements Comparable<Habitacion> {
 
 
     /**
-     * Este metodo cambia el valor de la variable estado a REPARACIÓN
+     * Este metodo cambia el valor de la variable estado a LIMPIEZA
      */
-    public void limpieza()
-    {
-        estado = "LIMPIEZA";
-    }
+    public void mantenimeinto() { estado = "MANTENIMIENTO"; }
 
 
     /**
-     * Este metodo cambia el valor de la variable estado a INACTIVO
+     * Este metodo cambia el valor de la variable estado a INACTIVA
      */
     public void inactiva()
     {
         estado = "INACTIVA";
     }
 
+
+    /**
+     * Este metodo permite eliminar la entidad de Habitacion
+     */
     @Action(semantics = NON_IDEMPOTENT_ARE_YOU_SURE)
     public void delete() {
         final String title = titleService.titleOf(this);
