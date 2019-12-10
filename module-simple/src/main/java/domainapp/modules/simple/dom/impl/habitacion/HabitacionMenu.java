ef3482f3bfbfcce440d5a7a37244966ed7296e15
cintiaMillacura2207 <cg2207m@gmail.com>
2019-12-10 16:04:36 -0300

    Issue #6:Se agrego la clase Menu
      Se agrego la clase Menu de la entidad.

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/habitacion/HabitacionMenu.java
@@ -0,0 +1,230 @@
+package domainapp.modules.simple.dom.impl.habitacion;
+
+import java.util.List;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.RestrictTo;
+import org.apache.isis.applib.annotation.SemanticsOf;
+
+import domainapp.modules.simple.dom.impl.enums.ListaHabitaciones;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        objectType = "Habitacion",
+        repositoryFor = Habitacion.class
+)
+@DomainServiceLayout(
+        named = "Habitacion",
+        menuOrder = "10"
+)
+/**
+ *
+ * Esta clase es el servicio de dominio Menu de la clase Habitacion
+ * que define los metodos
+ * que van a aparecer en el menu del sistema
+ *
+ *@author Cintia Millacura
+ *
+ */
+public class HabitacionMenu {
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Habitaciones"
+    )
+    @MemberOrder(sequence = "1")
+    /**
+     * Este metodo lista todos las Habitaciones que hay cargados
+     * en el sistema
+     *
+     * @return List<Habitacion>
+     */
+    public List<Habitacion> listarHabitaciones() {
+        return habitacionrepository.listarHabitaciones();
+    }
+
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Habitaciones Disponibles"
+    )
+    @MemberOrder(sequence = "2")
+    /**
+     * Este metodo lista todas las Habitaciones Disponibles que hay cargados
+     * en el sistema
+     *
+     * @return List<Habitacion>
+     */
+    public List<Habitacion> listarHabitacionesDisponibles() {
+        return habitacionrepository.listarHabitacionesDisponibles();
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Habitaciones Ocupadas"
+    )
+    @MemberOrder(sequence = "3")
+    /**
+     * Este metodo lista todas las Habitaciones Ocupadas que hay cargados
+     * en el sistema
+     *
+     * @return List<Habitacion>
+     */
+    public List<Habitacion> listarHabitacionesOcupadas() {
+        return habitacionrepository.listarHabitacionesOcupadas();
+    }
+
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Habitaciones Simples"
+    )
+    @MemberOrder(sequence = "4")
+    /**
+     * Este metodo lista todas las Habitaciones Simples que hay cargados
+     * en el sistema
+     *
+     * @return List<Habitacion>
+     */
+    public List<Habitacion> listarHabitacionesSimples() {
+        return habitacionrepository.listarHabitacionesSimples();
+    }
+
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Habitaciones Ejecutivas"
+    )
+    @MemberOrder(sequence = "5")
+    /**
+     * Este metodo lista todas las Habitaciones Ejecutivas que hay cargados
+     * en el sistema
+     *
+     * @return List<Habitacion>
+     */
+    public List<Habitacion> listarHabitacionesEjecutivas(){
+        return habitacionrepository.listarHabitacionesEjecutivas();
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Habitaciones Estandar"
+    )
+    @MemberOrder(sequence = "6")
+    /**
+     * Este metodo lista todas las Habitaciones Estandar que hay cargados
+     * en el sistema
+     *
+     * @return List<Habitacion>
+     */
+    public List<Habitacion> listarHabitacionesEstandar(){
+        return habitacionrepository.listarHabitacionesEstandar();
+    }
+
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Buscar Habitacion por Numero"
+    )
+    @MemberOrder(sequence = "7")
+    /**
+     * Este metodo permite encontrar una Habitacion en particular
+     * dado un numero que identifica de manera
+     * unica a cada Habitacion
+     *
+     * @param nombre
+     * @return List<Habitacion>
+     */
+    public List<Habitacion> buscarHabitacionPorNombre(
+            @ParameterLayout(named="Nombre")
+            final String nombre){
+        return habitacionrepository.buscarHabitacionPorNombre(nombre);
+    }
+
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Crear Habitacion"
+    )
+    @MemberOrder(sequence = "8")
+    /**
+     * Este metodo permite crear la entidad de dominio Habitacion
+     * con los datos que va a ingresar el usuario
+     *
+     * @param nombre
+     * @param ubicacion
+     * @param categoris
+     *
+     * @return Habitacion
+     *
+     */
+    public Habitacion crearHabitacion(
+            @ParameterLayout(named="Nombre") final String nombre,
+            @ParameterLayout(named="Ubicacion")final String ubicacion,
+            @ParameterLayout(named="Categoria") ListaHabitaciones categoria
+    ){
+        return habitacionrepository.crearHabitacion(nombre,ubicacion,categoria);
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Exportar PDF Lista de Habitaciones Disponibles"
+    )
+    @MemberOrder(sequence = "9")
+    /**
+     * Este metodo permite generar un reporte en formato PDF del listado de las Habitaciones Disponibles
+     * en total que hay registrados en el sistema
+     *
+     */
+    public void generarReporteHabitacionesDisponibles(
+    ) {
+        habitacionrepository.generarReporteHabitacionesDisponibles();
+    }
+
+
+    @javax.inject.Inject
+    HabitacionRepository habitacionrepository;
+}
