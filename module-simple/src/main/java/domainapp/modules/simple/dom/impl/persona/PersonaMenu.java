80145f6858b8a50271806e4bd8fe71b039106d0c
cintiaMillacura2207 <cg2207m@gmail.com>
2019-12-10 16:02:29 -0300

    Issue #1:Se agrego clase Menu
      Se agrego la clase Menu de la entidad.

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/persona/PersonaMenu.java
@@ -0,0 +1,233 @@
+package domainapp.modules.simple.dom.impl.persona;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.RestrictTo;
+import org.apache.isis.applib.annotation.SemanticsOf;
+
+import domainapp.modules.simple.dom.impl.enums.ListaJerarquias;
+import domainapp.modules.simple.dom.impl.enums.TipoSexo;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        objectType = "Persona",
+        repositoryFor = Persona.class
+)
+@DomainServiceLayout(
+        named = "Personas",
+        menuOrder = "10"
+)
+/**
+ *
+ * Esta clase es el servicio de dominio Menu de la clase Persona
+ * que define los metodos
+ * que van a aparecer en el menu del sistema
+ *
+ *@author Cintia Millacura
+ *
+ */
+public class PersonaMenu {
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Personas"
+    )
+    @MemberOrder(sequence = "1")
+    /**
+     * Este metodo lista todos las personas que hay registradas
+     * en el sistema
+     *
+     * @return List<Persona>
+     */
+    public List<Persona> listarPersonas() {
+        return personarepository.listarPersonas();
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Personas Ejecutivas"
+    )
+    @MemberOrder(sequence = "2")
+    /**
+     * Este metodo lista todas las personas con jerarquia de Ejecutivos que hay cargados
+     * en el sistema
+     *
+     * @return List<Persona>
+     */
+    public List<Persona> listarPersonasEjecutivas() {
+        return personarepository.listarPersonasEjecutivas();
+    }
+
+    @Action(
+    semantics = SemanticsOf.SAFE,
+    restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Personas Supervisores"
+    )
+    @MemberOrder(sequence = "3")
+    /**
+     * Este metodo lista todas las personas con jerarquia de Supervisores que hay cargados
+     * en el sistema
+     *
+     * @return List<Persona>
+     */
+    public List<Persona> listarPersonasSupervisores() {
+        return personarepository.listarPersonasSupervisores();
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Listado de Personas Operadores"
+    )
+    @MemberOrder(sequence = "4")
+    /**
+     * Este metodo lista todas las personas con jerarquia de Operadores que hay cargados
+     * en el sistema
+     *
+     * @return List<Persona>
+     */
+    public List<Persona> listarPersonasOperadores() {
+        return personarepository.listarPersonasOperadores();
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Buscar Persona por Nombre"
+    )
+    @MemberOrder(sequence = "5")
+    /**
+     * Este metodo permite encontrar una Persona en particular
+     * dado un nombre
+     *
+     * @param nombre
+     * @return List<Persona>
+     */
+    public List<Persona> buscarPersonaPorNombre(
+            @ParameterLayout(named = "Nombre") final String nombre
+    ) {
+        return personarepository.buscarPersonaPorNombre(nombre);
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Crear Persona"
+    )
+    @MemberOrder(sequence = "6")
+    /**
+     * Este metodo permite crear la entidad de dominio Persona
+     * con los datos que va a ingresar el usuario
+     *
+     *
+     * @param nombre
+     * @param apellido
+     * @param direccion
+     * @param telefono
+     * @param email
+     * @param dni
+     * @param jerarquias
+     *
+     *
+     * @return Persona
+     */
+    public Persona crearPersona(
+
+            @Parameter(
+                    regexPattern = "[A-Za-z ]+",
+                    regexPatternFlags = Pattern.CASE_INSENSITIVE,
+                    regexPatternReplacement = "Ingrese dato correcto"
+            )
+            @ParameterLayout(named = "Nombre") final String nombre,
+            @Parameter(
+                    regexPattern = "[A-Za-z ]+",
+                    regexPatternFlags = Pattern.CASE_INSENSITIVE,
+                    regexPatternReplacement = "Ingrese dato correcto"
+            )
+            @ParameterLayout(named = "Apellido") final String apellido,
+            @Parameter(
+                    regexPattern = "[A-Za-z ]+[0-9]+",
+                    regexPatternFlags = Pattern.CASE_INSENSITIVE,
+                    regexPatternReplacement = "Ingrese dato correcto"
+            )
+            @ParameterLayout(named = "Direccion") final String direccion,
+            @Parameter(
+                    regexPattern = "[0-9]+",
+                    regexPatternFlags = Pattern.CASE_INSENSITIVE,
+                    regexPatternReplacement = "Ingrese dato correcto"
+            )
+            @ParameterLayout(named = "Telefono") final String telefono,
+            @Parameter(
+                    regexPattern = "(\\w+\\.)*\\w+@(\\w+\\.)+[A-Za-z]+",
+                    regexPatternFlags = Pattern.CASE_INSENSITIVE,
+                    regexPatternReplacement = "Ingrese una dirección de correo electrónico válida (contienen un símbolo '@') -"
+            )
+            @ParameterLayout(named = "Email") final String email,
+            @Parameter(
+                    regexPattern = "[0-9]+",
+                    regexPatternFlags = Pattern.CASE_INSENSITIVE,
+                    regexPatternReplacement = "Ingrese dato correcto"
+            )
+            @ParameterLayout(named = "Dni") final String dni,
+            @ParameterLayout(named = "Jerarquia") ListaJerarquias jerarquias,
+            @ParameterLayout(named = "Sexo") TipoSexo sexo
+
+
+    ) {
+        return personarepository.crearPersona(nombre,apellido,direccion,telefono,email,dni,jerarquias,sexo);
+    }
+
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = RestrictTo.PROTOTYPING
+    )
+    @ActionLayout(
+            bookmarking = BookmarkPolicy.AS_ROOT,
+            named = "Exportar PDF Lista de Personas"
+    )
+    @MemberOrder(sequence = "7")
+    /**
+     * Este metodo permite generar un reporte en formato PDF del listado de personas
+     * en total que hay registrados en el sistema
+     *
+     */
+    public void generarReportePersonas(
+    ) {
+         personarepository.generarReportePersonas();
+        //return (Blob)personarepository.generarReportePersonas();
+    }
+
+
+    @javax.inject.Inject
+    PersonaRepository personarepository;
+}
